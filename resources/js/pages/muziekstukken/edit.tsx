469ec0097fb38d9c0a7396a1dc165b831c7e8660
JoranOut <jorankorverout@digiboox.app>
2026-05-03 01:21:41 +0200

    Remove unused Label import from edit page

--- resources/js/pages/muziekstukken/edit.tsx
+++ resources/js/pages/muziekstukken/edit.tsx
@@ -15,7 +15,6 @@ import {
     DialogTitle,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import {
     Table,
     TableBody,
